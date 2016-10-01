a73879133d63f4cb1fb7ccae1db2214e8a9d77d2
eti-yen <emeraldwatch15@gmail.com>
2016-10-01 20:20:17 +0100

    Unity project added

---
+++ Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    Transform trans;
+
+    // Use this for initialization
+    void Start()
+    {
+        trans = GetComponent<Transform>();
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 movement = new Vector2();
+        movement.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        movement.y = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        trans.Translate(movement);
+    }
+}
