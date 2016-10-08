db31ffe09307ecdd925b73b44c799ef66e1f90a4
eti-yen <emeraldwatch15@gmail.com>
2016-10-08 18:32:35 +0100

    Added attacks, made things bouncy

---
+++ Game/Assets/Scripts/Attack.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attack : MonoBehaviour
+{
+    public float force = 5f;
+
+    // Use this for initialization
+    void Start()
+    {
+	
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+	
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Rigidbody2D hitThing = other.gameObject.GetComponent<Rigidbody2D>();
+        if (hitThing != null)
+            other.GetComponent<Rigidbody2D>().AddForce(Vector2.right * force, ForceMode2D.Impulse);
+    }
+}
--- Game/Assets/Scripts/PlayerController.cs
+++ Game/Assets/Scripts/PlayerController.cs
@@ -4,21 +4,29 @@ using System.Collections;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public GameObject attack;
 
     Transform trans;
+    Rigidbody2D rb2d;
 
     // Use this for initialization
     void Start()
     {
         trans = GetComponent<Transform>();
+        rb2d = GetComponent<Rigidbody2D>();
     }
 	
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         Vector2 movement = new Vector2();
         movement.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
         movement.y = Input.GetAxis("Vertical") * speed * Time.deltaTime;
-        trans.Translate(movement);
+        rb2d.AddForce(movement, ForceMode2D.Impulse);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject hit = (GameObject)Instantiate(attack, trans.position + new Vector3(0.7f, 0f, 0f), Quaternion.identity, trans);
+            Destroy(hit, 0.1f);
+        }
     }
 }
